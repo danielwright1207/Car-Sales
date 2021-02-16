7de3979b796f3ac7817858d136e8442328b52400
Daniel Wright <daniel.wright1207@gmail.com>
2021-02-16 12:48:35 -0700

    final after flexing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
-// import state from "./reducers/featureListReducer";
+
 const App = () => {
   // const state = {
   //   additionalPrice: 0,
